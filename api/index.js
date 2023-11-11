83b2c04cf98d266401f01a312b22e75743cba93d
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2023-11-12 00:08:16 +0200

    add database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app =express();
+const app = express();
+dotenv.config();
 
-app.listen(3000,()=>{
-console.log(`server connected in a port 3000`);
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log("DB Connetion Successfull");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`server connected in a port ${process.env.PORT}`);
 })
