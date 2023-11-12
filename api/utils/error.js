821fb96aa5240baf1421acc84ded850a6ff246b6
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2023-11-12 16:05:52 +0200

    complete signup page funcionality

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,7 +1,7 @@
 export const errorHandler = (statusCode, message) => {
     const error = new Error();
     error.statusCode = statusCode;
-    error.message = message
+    error.message = message;
 
     return error;
 }
