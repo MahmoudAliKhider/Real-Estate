aee640dd026a60490b916c58a5fe6d82b561ba19
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2023-11-18 23:41:43 +0200

    create update listing route

--- api/routes/listing.controller.js
+++ api/routes/listing.controller.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createList, deleteList } from '../controllers/listing.controller.js';
+import { createList, deleteList, updateList } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createList);
 router.delete("/delete/:id", verifyToken, deleteList);
+router.post("/update/:id", verifyToken, updateList)
 
 export default router;
